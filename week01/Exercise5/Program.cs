a6a164022ef386e9d86269f4646d4638660bd8c5
gitvuyelwa <vmacanda@byupathway.edu>
2025-05-09 11:44:36 +0200

    update

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+       DisplayWelcomeMessage();
+       string userName = GetUserName();
+       int userNumber = GetUserNumber();
+       int squaredNumber = SquareNumber(userNumber);
+        DisplayResults(userName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the number squaring program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int GetUserNumber()
+    {
+        Console.Write("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squaredNumber = number * number;
+        return squaredNumber;
+    }
+
+    static void DisplayResults(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"Hello {userName}, the square of your number is: {squaredNumber}");
     }
 }
