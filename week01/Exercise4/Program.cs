77ff6d903275d7f4a5eae66bd02f2995d881ecd0
gitvuyelwa <vmacanda@byupathway.edu>
2025-05-09 11:26:38 +0200

    update

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine(numbers.Sum());
+        Console.WriteLine("The sum is: " + numbers.Sum());
+        Console.WriteLine("The average is: " + numbers.Average());
+        Console.WriteLine("The largest number is: " + numbers.Max());
+    
     }
 }
