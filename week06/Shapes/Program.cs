6f83a899c116caa05c65f86eb7e048bf938d4731
gitvuyelwa <vmacanda@byupathway.edu>
2025-06-11 15:18:42 +0200

    update

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s = new Square("Red", 5);
+        shapes.Add(s);
+
+        Rectangle r = new Rectangle("Blue", 4, 6);
+        shapes.Add(r);
+
+        Circle c = new Circle("Green", 3);
+        shapes.Add(c);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
